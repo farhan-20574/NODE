586f31d58a3c6dcc448ab61741ea3bb18a7ea90b
farhan-20574 <farhan20574@outlook.com>
2025-05-25 22:36:32 +0500

    folder sturcture for NODE project

--- Practice/JavaScript training/201-02-ex01.js
+++ Practice/JavaScript training/201-02-ex01.js
@@ -1,10 +1,12 @@
 //Condition Exercise # 01
 
+const isLoggedIn = true;
+const isProSubscriber = false;
 
-const isLoggedIn=true
-const isProSubscriber=true
-
-
-if (!=isLoggedIn){
-    console.log()
+if (!isLoggedIn) {
+  console.log("please login first");
+} else if (isLoggedIn && !isProSubscriber) {
+  console.log('here is your data, consider subscribing to get access to more features');
+} else if (isLoggedIn && isProSubscriber) {
+  console.log("Thanks, you are a pro subscriber.");
 }
